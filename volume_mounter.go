862c425f53a86097c437594c265af7b848d2878c
Dennis Leon <dleon@pivotal.io>
2019-05-14 11:47:09 -0700

    Remove unused code
    [#165727764] https://www.pivotaltracker.com/story/show/165727764
    
    Signed-off-by: Paul Warren <paul.warren@emc.com>
    Co-authored-by: Paul Warren <paul.warren@emc.com>

--- volume_mounter.go
+++ volume_mounter.go
@@ -1,13 +1,7 @@
 package volumedriver
 
 import (
-	"context"
-	"fmt"
-	"time"
-
 	"code.cloudfoundry.org/dockerdriver"
-	"code.cloudfoundry.org/dockerdriver/driverhttp"
-	"code.cloudfoundry.org/dockerdriver/invoker"
 )
 
 //go:generate counterfeiter -o volumedriverfakes/fake_mounter.go . Mounter
@@ -16,41 +10,4 @@ type Mounter interface {
 	Unmount(env dockerdriver.Env, target string) error
 	Check(env dockerdriver.Env, name, mountPoint string) bool
 	Purge(env dockerdriver.Env, path string)
-}
-
-type volumeMounter struct {
-	invoker     invoker.Invoker
-	fstype      string
-	defaultOpts string
-}
-
-func NewVolumeMounter(invoker invoker.Invoker, fstype, defaultOpts string) Mounter {
-	return &volumeMounter{invoker, fstype, defaultOpts}
-}
-
-func (m *volumeMounter) Mount(env dockerdriver.Env, source string, target string, opts map[string]interface{}) error {
-	_, err := m.invoker.Invoke(env, "mount", []string{"-t", m.fstype, "-o", m.defaultOpts, source, target})
-	return err
-}
-
-func (m *volumeMounter) Unmount(env dockerdriver.Env, target string) error {
-	_, err := m.invoker.Invoke(env, "umount", []string{target})
-	return err
-}
-
-func (m *volumeMounter) Check(env dockerdriver.Env, name, mountPoint string) bool {
-	ctx, cncl := context.WithDeadline(context.TODO(), time.Now().Add(time.Second*5))
-	defer cncl()
-	env = driverhttp.EnvWithContext(ctx, env)
-	_, err := m.invoker.Invoke(env, "mountpoint", []string{"-q", mountPoint})
-
-	if err != nil {
-		env.Logger().Info(fmt.Sprintf("unable to verify volume %s (%s)", name, err.Error()))
-		return false
-	}
-	return true
-}
-
-func (m *volumeMounter) Purge(_ dockerdriver.Env, _ string) {
-	// this is a no-op for now
 }
