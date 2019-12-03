a7ce60da2a994402310d8572797c3ec1a72efedc
Dennis Leon <dleon@pivotal.io>
2019-12-03 11:41:01 -0800

    Test code refactor
    Co-authored-by: Dennis Leon <dleon@pivotal.io>
    Co-authored-by: Lisa Cho <lcho@pivotal.io>

--- invoker/invoker_suite_test.go
+++ invoker/invoker_suite_test.go
@@ -4,8 +4,6 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	"fmt"
-	"io"
 	"testing"
 )
 
@@ -13,25 +11,3 @@ func TestInvoker(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Invoker Suite")
 }
-
-var _ = SynchronizedBeforeSuite(func() []byte {
-	return nil
-}, func(pathsByte []byte) {
-
-})
-
-var _ = SynchronizedAfterSuite(func() {
-
-}, func() {
-})
-
-// testing support types:
-
-type errCloser struct{ io.Reader }
-
-func (errCloser) Close() error                     { return nil }
-func (errCloser) Read(p []byte) (n int, err error) { return 0, fmt.Errorf("any") }
-
-type stringCloser struct{ io.Reader }
-
-func (stringCloser) Close() error { return nil }
